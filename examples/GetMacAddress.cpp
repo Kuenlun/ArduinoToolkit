c14a68207facb5836ae2acc4a7ff425f14a7a3bd
Kuenlun <kuenlun13054@gmail.com>
2022-06-07 13:09:43 +0200

    Added "GetMacAddress.cpp" program to get the ESP MAC address

---
+++ examples/GetMacAddress.cpp
@@ -0,0 +1,30 @@
+#include <WiFi.h>
+#include <Tools/Logger.h>
+#include <Tools/TimedExecution.h>
+
+
+void printMACAddress() { LOG_INFO(WiFi.macAddress()); }
+
+
+/* * * * * *
+ *  SETUP  *
+ * * * * * */
+void setup()
+{
+    // Configure logger
+    Serial.begin(115200);
+    Tools::logger.config("GetMACAddress");
+
+    // Configure WiFi
+    WiFi.mode(WIFI_MODE_STA);
+}
+
+
+/* * * * * *
+ *  LOOP   *
+ * * * * * */
+void loop()
+{
+    // Print the MAC address every 5 seconds
+    TIMED_EXECUTION(Tools::secsToMillis(5), printMACAddress);
+}
