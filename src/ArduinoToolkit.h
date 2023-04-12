81fb3277c02f8087a802aeeeaf213b13a6dbe328
Kuenlun <kuenlun13054@gmail.com>
2023-04-12 14:27:00 +0200

    WiFi daemon implemented without class

--- src/ArduinoToolkit.h
+++ src/ArduinoToolkit.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "ArduinoToolkit/Core/Assert.h"
+#include "ArduinoToolkit/Core/Log.h"
+
 #include "ArduinoToolkit/WiFi/WiFiDaemon.h"
 #include "ArduinoToolkit/WiFi/NTPClientDaemon.h"
 #include "ArduinoToolkit/WiFi/OTA_AWS_S3.h"
